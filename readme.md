f84bf43857e336df0590cdf1f5fd3bd55f3ad644
Tim MacDonald <tim.mac7@me.com>
2018-02-28 20:31:12 +1100

    initial commit

---
+++ readme.md
@@ -0,0 +1,58 @@
+# Multisite Backup Command for spatie/laravel-backup
+
+This command wraps the `spatie/laravel-backup` backup command to make it super simple to backup multiple sites on a single server - from the one Laravel installation. You can read more about is [over on my blog](https://timacdonald.me/backup-multiple-sites-frameworks-laravel-backup/).
+
+## Installation
+
+You should already have `spatie/laravel-backup` installed and configured.
+
+You can install using [composer](https://getcomposer.org/) from [Packagist](https://packagist.org/packages/timacdonald/multisite-backup-command).
+
+```
+composer require timacdonald/multisite-backup-command
+```
+
+## Versioning
+
+This package uses *Semantic Versioning*. You can find out more about what this is and why it matters by reading [the spec](http://semver.org) or for something more readable, check out [this post](https://laravel-news.com/building-apps-composer).
+
+## Basic Usage
+
+Extend the class and add the command to your app as per usual. Then add you sites the `$sites` variable:
+
+```php
+<?php
+
+namespace App\Console\Commands;
+
+use TiMacDonald\MultisiteBackupCommand\BackupCommand as BaseBackupCommand;
+
+class BackupCommand extends BaseBackupCommand
+{
+    protected $sites = [
+        [
+            'domain' => 'timacdonald.com.au',
+            'database' => 'timacdonald',
+            'paths' => ['storage/app'],
+        ],
+        [
+            'domain' => 'my.wordpress.site.com.au',
+            'database' => 'my_wp_db',
+            'paths' => ['wp-content/uploads'],
+        ],
+    ];
+}
+
+```
+
+## Contributing
+
+Please feel free to suggest new ideas or send through pull requests to make this better. If you'd like to discuss the project, feel free to reach out on [Twitter](https://twitter.com/timacdonald87). I just throw my ideas for the project in the [issues list](https://github.com/timacdonald/immutable-carbon/issues) if you want to help implement anything.
+
+## License
+
+This package is under the MIT License. See [LICENSE](https://github.com/timacdonald/immutable-carbon/blob/master/LICENSE) file for details.
+
+## Thanks
+
+Big thanks to Spatie for providing such a great backup package!
---
+++ src/BackupCommand.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace TiMacDonald\MultisiteBackupCommand;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Config\Repository;
+
+class BackupCommand extends Command
+{
+    protected $config;
+
+    protected $sites = [];
+
+    public function __construct(Repository $config)
+    {
+        $this->config = $config;
+
+        parent::__construct();
+    }
+
+    protected $signature = 'app:backup {--clean} {--list} {--monitor} {--run}';
+
+    protected $description = 'Backup all the sites';
+
+    public function handle()
+    {
+        if ($this->isSingleSiteCommand()) {
+            $this->runSingleSiteCommand();
+            return;
+        }
+        $this->runMultiSiteCommand();
+    }
+
+    protected function isSingleSiteCommand()
+    {
+        return in_array($this->backupType(), ['clean', 'run']);
+    }
+
+    protected function runSingleSiteCommand()
+    {
+        collect($this->sites)->each(function ($site) {
+            $this->setupSingleSiteConfig($site);
+            $this->callBackupCommand();
+        });
+    }
+
+    protected function runMultiSiteCommand()
+    {
+        $this->setupMultiSiteConfig();
+        $this->callBackupCommand();
+    }
+
+    protected function setupSingleSiteConfig($site)
+    {
+        $this->config->set('backup.backup.name', $this->siteName($site));
+
+        $this->config->set('database.connections.mysql.database', $site['database']);
+
+        $this->config->set('backup.backup.source.files.include', $this->siteIncludes($site));
+    }
+
+    protected function setupMultiSiteConfig()
+    {
+        $this->config->set('backup.monitorBackups', $this->configForMultiSite());
+    }
+
+    protected function configForMultiSite()
+    {
+        return array_map(function ($site) {
+            return array_merge($this->config->get('backup.monitorBackups.0'), [
+                'name' => $this->siteName($site),
+            ]);
+        }, $this->sites);
+    }
+
+    protected function siteName($site)
+    {
+        return 'https://'.$site['domain'];
+    }
+
+    protected function siteIncludes($site)
+    {
+        return array_map(function ($path) use ($site) {
+            return base_path("../{$site['domain']}/$path");
+        }, $site['paths']);
+    }
+
+    protected function callBackupCommand()
+    {
+        $this->call('backup:'.$this->backupType());
+    }
+
+    protected function backupType()
+    {
+        return collect($this->options())
+            ->only(['run', 'clean', 'list', 'monitor'])
+            ->filter()
+            ->keys()
+            ->first() ?? 'run';
+    }
+}
